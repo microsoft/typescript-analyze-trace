a87a920e2f9828bebc23498f150ca806adc309fa
Andrew Casey <andrew.casey@microsoft.com>
2022-01-20 18:34:36 -0800

    Adopt yargs in analyze-trace-file

--- src/analyze-trace-file.ts
+++ src/analyze-trace-file.ts
@@ -1,38 +1,53 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-if (process.argv.length !== 3 && process.argv.length != 4) {
-    const path = require("path");
-    console.error(`Usage: ${path.basename(process.argv[0])} ${path.basename(process.argv[1])} trace_path [type_path]`);
-    process.exit(1);
-}
-
 import { assert } from "console";
 import chalk = require("chalk");
 import treeify = require("treeify");
 import fs = require("fs");
 import path = require("path");
+import yargs = require("yargs");
 
 import getTypeTree = require("./get-type-tree");
 import normalizePositions = require("./normalize-positions");
 
+const argv = yargs(process.argv.slice(2))
+    .command("$0 <tracePath> [typesPath]", "Preprocess tracing type dumps", yargs => yargs
+        .positional("tracePath", { type: "string", desc: "Trace file to read", coerce: throwIfNotFile })
+        .positional("typesPath", { type: "string", desc: "Corresponding types file", coerce: throwIfNotFile })
+        .options({
+            "forceMillis": {
+                alias: ["forcemillis", "force-millis"],
+                describe: "Events of at least this duration (in milliseconds) will reported unconditionally",
+                type: "number",
+                default: 500,
+
+            },
+            "skipMillis": {
+                alias: ["skipmillis", "skip-millis"],
+                describe: "Events of less than this duration (in milliseconds) will suppressed unconditionally",
+                type: "number",
+                default: 100,
+
+            },
+        })
+        .check(argv => {
+            if (argv.forceMillis < argv.skipMillis) {
+                throw new Error("forceMillis cannot be less than skipMillis")
+            }
+            return true;
+        })
+        .help("h").alias("h", "help")
+        .strict().skipValidation)
+    .argv;
+
 const Parser = require("jsonparse");
 
-const tracePath = process.argv[2];
-const typesPath = process.argv[3];
+const tracePath = argv.tracePath!;
+const typesPath = argv.typesPath;
 
-if (!fs.existsSync(tracePath)) {
-    console.error(`${tracePath} does not exist`);
-    process.exit(2);
-}
-
-if (typesPath && !fs.existsSync(typesPath)) {
-    console.error(`${typesPath} does not exist`);
-    process.exit(3);
-}
-
-const thresholdDuration = 5E5; // microseconds
-const minDuration = 1E5; // microseconds
+const thresholdDuration = argv.forceMillis * 1000; // microseconds
+const minDuration = argv.skipMillis * 1000; // microseconds
 const minPercentage = 0.6;
 
 main().catch(err => console.error(`Internal Error: ${err.message}\n${err.stack}`));
@@ -290,7 +305,7 @@ let typesCache: undefined | readonly any[];
 async function getTypes(): Promise<readonly any[]> {
     if (!typesCache) {
         try {
-            const json = await fs.promises.readFile(typesPath, { encoding: "utf-8" });
+            const json = await fs.promises.readFile(typesPath!, { encoding: "utf-8" });
             typesCache = JSON.parse(json);
         }
         catch (e: any) {
@@ -432,4 +447,11 @@ function unmangleCamelCase(name: string) {
 
 function getLineCharMapKey(line: number, char: number) {
     return `${line},${char}`;
+}
+
+function throwIfNotFile(path: string): string {
+    if (!fs.existsSync(path) || !fs.statSync(path)?.isFile()) {
+        throw new Error(`${path} is not a file`);
+    }
+    return path;
 }
